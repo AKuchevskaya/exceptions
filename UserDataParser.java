80a6005e031fedbc1f1ee2bc98fed46cb476db82
AKuchevskaya <alena.kuchevskaya@yandex.ru>
2024-04-16 15:13:37 +0300

    Добавила задание и возможные импорты

---
+++ UserDataParser.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java. time. LocalDate;
+
+public class UserDataParser {
+    public static void main(String[] args) {
+    }
+}
